daaddb97e9d3a7ee14ed2a120bfbb7f3e0295376
martijnbentum <bentummartijn@gmail.com>
2023-11-09 17:27:21 +0100

    module with path and filenames

---
+++ locations.py
@@ -0,0 +1,17 @@
+import glob
+
+base = '../'
+scans = base + 'kadaster_scans/'
+with_stamp = scans + 'with_stamp/'
+no_stamp = scans + 'no_stamp/'
+with_note = scans + 'with_note/'
+no_note = scans + 'no_note/'
+
+
+no_stamp_files = glob.glob(no_stamp + '*.jpg')
+with_stamp_files = glob.glob(with_stamp + '*.jpg')
+no_note_files = glob.glob(no_note + '*.jpg')
+with_note_files = glob.glob(with_note + '*.jpg')
+
+
+
