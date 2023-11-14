cc3180e99ad176dafc14395ac60c7263748e7faa
martijnbentum <bentummartijn@gmail.com>
2023-11-14 14:40:55 +0100

    added all files with all kadaster scan filenames, added cropped image directories; added viti outputs (outputs from google image classification model) directory

--- locations.py
+++ locations.py
@@ -8,11 +8,18 @@ with_note = scans + 'with_note/'
 no_note = scans + 'no_note/'
 
 pickle = base + 'kadaster_pickle/'
+cropped_name = base + 'kadaster_cropped_name/'
+cropped_top = base + 'kadaster_cropped_top/'
+
+viti_outputs= base + 'kadaster_viti_outputs/'
 
 no_stamp_files = glob.glob(no_stamp + '*.jpg')
 with_stamp_files = glob.glob(with_stamp + '*.jpg')
 no_note_files = glob.glob(no_note + '*.jpg')
 with_note_files = glob.glob(with_note + '*.jpg')
+
+all_files = no_stamp_files + with_stamp_files + no_note_files + with_note_files
+
 
 
 
