c5384b4c93a62fe6a751107c020e9ff441dc2954
martijnbentum <bentummartijn@gmail.com>
2023-11-22 15:15:28 +0100

    added kraken locations

--- locations.py
+++ locations.py
@@ -12,6 +12,11 @@ cropped_name = base + 'kadaster_cropped_name/'
 cropped_top = base + 'kadaster_cropped_top/'
 
 viti_outputs= base + 'kadaster_viti_outputs/'
+kraken_outputs = base + 'kadaster_kraken/'
+kraken_no_stamp = kraken_outputs + 'no_stamp/'
+kraken_with_stamp = kraken_outputs + 'with_stamp/'
+kraken_no_note = kraken_outputs + 'no_note/'
+kraken_with_note = kraken_outputs + 'with_note/'
 
 no_stamp_files = glob.glob(no_stamp + '*.jpg')
 with_stamp_files = glob.glob(with_stamp + '*.jpg')
@@ -20,6 +25,14 @@ with_note_files = glob.glob(with_note + '*.jpg')
 
 all_files = no_stamp_files + with_stamp_files + no_note_files + with_note_files
 
+
+kraken_no_stamp_files = glob.glob(kraken_no_stamp + '*.json')
+kraken_with_stamp_files = glob.glob(kraken_with_stamp + '*.json')
+kraken_no_note_files = glob.glob(kraken_no_note + '*.json')
+kraken_with_note_files = glob.glob(kraken_with_note + '*.json')
+
+all_kraken_files =  kraken_with_note_files + kraken_no_note_files
+all_kraken_files +=  kraken_with_stamp_files + kraken_no_stamp_files
 
 
 
